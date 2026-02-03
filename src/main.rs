f8032745e582e7beac2e72c97fc9cd31b50e0b9d
Yeabsirashimelis <shimelisyeabsiragithub@gmail.com>
2026-02-03 13:51:11 +0300

    feat: the application register functionality

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,65 @@
 use std::env;
 
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
-use sqlx::Connection;
+use actix_web::{App, HttpResponse, HttpServer, Responder, http::header::ContentType, web};
+use serde::Deserialize;
+use sqlx::PgPool;
 
 //db connection
-async fn connect_db() {
+async fn connect_db() -> Result<sqlx::PgPool, sqlx::Error> {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let mut conn = sqlx::postgres::PgConnection::connect(&database_url).await;
+    let pool = PgPool::connect(&database_url).await?;
+
+    Ok(pool)
 }
 
-async fn post_program() -> impl Responder {
+#[derive(serde::Deserialize, sqlx::Type, Debug)]
+#[sqlx(type_name = "app_status", rename_all = "UPPERCASE")]
+enum AppStatus {
+    PENDING,
+    RUNNING,
+    STOPPED,
+    FAILED,
+}
+
+#[derive(Deserialize, Debug)]
+struct Application {
+    name: String,
+    command: String,
+    status: AppStatus,
+    port: i32,
+}
+
+async fn post_program(pool: web::Data<PgPool>, app: web::Json<Application>) -> impl Responder {
+    println!("{:?}", app);
+    let query = "INSERT INTO App (name, command, status, port) VALUES ($1, $2, $3, $4)";
+    sqlx::query(query)
+        .bind(&app.name)
+        .bind(&app.command)
+        .bind(&app.status)
+        .bind(&app.port)
+        .execute(pool.get_ref())
+        .await
+        .unwrap();
+
     HttpResponse::Ok()
+        .content_type(ContentType::plaintext())
+        .body("application registered successfully")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    //load the environmnet variables at the start of the server
+    dotenvy::dotenv().ok();
+
     let addr = ("127.0.0.1", 8080);
+    let pool = connect_db().await.expect("DB connection failed");
     println!("app is bound to http://{}:{}", addr.0, addr.1);
-    HttpServer::new(|| App::new().route("/", web::post().to(post_program)))
-        .bind(addr)?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .route("/", web::post().to(post_program))
+    })
+    .bind(addr)?
+    .run()
+    .await
 }
