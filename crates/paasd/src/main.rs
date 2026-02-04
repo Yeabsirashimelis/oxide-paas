bd9d5b5f99949214129cd2cec22a51e836ee9a7e
Yeabsirashimelis <shimelisyeabsiragithub@gmail.com>
2026-02-04 21:53:55 +0300

    feat: get a single application information

--- crates/paasd/src/main.rs
+++ crates/paasd/src/main.rs
@@ -4,7 +4,7 @@ mod repository;
 
 use std::env;
 
-use crate::handlers::app_handlers::{get_programs, post_program};
+use crate::handlers::app_handlers::{get_program, get_programs, post_program};
 use actix_web::{App, HttpServer, web};
 use sqlx::PgPool;
 
@@ -27,8 +27,9 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
-            .route("/", web::post().to(post_program))
-            .route("/", web::get().to(get_programs))
+            .route("/apps", web::post().to(post_program))
+            .route("/apps", web::get().to(get_programs))
+            .route("/apps/{app_id}", web::get().to(get_program))
     })
     .bind(addr)?
     .run()
